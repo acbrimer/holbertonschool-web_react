424d1314a6d5cde6a2f702af9a761f3b53dbf3df
Adam <arbimer@mac.com>
2021-07-27 13:06:24 -0500

    Added solution for task 10

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,6 +1,24 @@
 /// <reference path="./subjects/Subject.ts" />
 
-console.log('Subject namespace: ', Subjects);
-console.log('CPP class: ', Subjects.Cpp);
-console.log('React class: ', Subjects.Cpp);
-console.log('Java class: ', Subjects.Cpp);
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+export const cTeacher:Subjects.Teacher = {
+    firstName: "Professor",
+    lastName: "C. Teacher",
+    experienceTeachingC: 10
+}
+cpp.teacher = cTeacher;
+console.log("C++")
+console.log("   getRequirements: ", cpp.getRequirements())
+console.log("   getAvailableTeacher: ", cpp.getAvailableTeacher())
+java.teacher = cTeacher;
+console.log("Java")
+console.log("   getRequirements: ", cpp.getRequirements())
+console.log("   getAvailableTeacher: ", cpp.getAvailableTeacher())
+react.teacher = cTeacher;
+console.log("React")
+console.log("   getRequirements: ", cpp.getRequirements())
+console.log("   getAvailableTeacher: ", cpp.getAvailableTeacher())
+
+
