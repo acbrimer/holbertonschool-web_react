6fa3700be200a2fc963d744842a8b64edb52e45f
Adam <arbimer@mac.com>
2021-07-27 12:57:20 -0500

    Addde solution for task 9 (I think?)

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,6 @@
+/// <reference path="./subjects/Subject.ts" />
+
+console.log('Subject namespace: ', Subjects);
+console.log('CPP class: ', Subjects.Cpp);
+console.log('React class: ', Subjects.Cpp);
+console.log('Java class: ', Subjects.Cpp);
