9c696cfa6bc2751da561ac1a9941b6298e18dc8c
Adam <arbimer@mac.com>
2022-03-10 20:15:48 -0600

    Added solution for task 1

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div id="root-notifications"></div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <main className="App-body">Login to access the full dashboard</main>
+      <footer className="App-footer">
+        <i>Copyright 2020 - holberton school</i>
+      </footer>
+    </>
+  );
+}
+
+export default App;
