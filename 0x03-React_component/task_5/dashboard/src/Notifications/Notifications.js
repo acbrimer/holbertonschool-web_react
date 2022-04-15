f937c8319c075061f01f15532ad3d2aa9fe3507e
Adam <arbimer@mac.com>
2022-04-15 10:24:11 -0500

    Updated Notifications to PureComponent for tasks 9 & 10

---
+++ 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,83 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import './Notifications.css';
+import NotificationItem from './NotificationItem';
+import NotificationItemShape from './NotificationItemShape';
+
+class Notifications extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return (
+      (this.props.listNotifications || []).length <
+      (nextProps.listNotifications || []).length
+    );
+  }
+
+  render() {
+    const { displayDrawer, listNotifications } = this.props;
+    const handleCloseClick = () => {
+      console.log('Close button has been clicked');
+    };
+    return (
+      <div className="notificationsWrapper">
+        <div className="menuItem">Your notifications</div>
+        <div
+          className={clsx('Notifications', {
+            ['hidden']: !displayDrawer,
+          })}
+        >
+          <button
+            onClick={handleCloseClick}
+            aria-label="Close"
+            style={{
+              boxShadow: 'unset',
+              position: 'absolute',
+              top: 12,
+              right: 12,
+              background: 'transparent',
+              border: 'unset',
+            }}
+          >
+            X
+          </button>
+          <p>Here is the list of notifications</p>
+          <ul>
+            {listNotifications.length === 0 ? (
+              <li>No new notification for now</li>
+            ) : (
+              listNotifications.map((notification) => (
+                <NotificationItem
+                  key={`notification-${notification.id}`}
+                  markAsRead={this.markAsRead}
+                  {...notification}
+                />
+              ))
+            )}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+Notifications.defaultProps = {
+  displayDrawer: true,
+  listNotifications: [],
+};
+
+export default Notifications;
