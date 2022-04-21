9f4006de4d8d8e23c649c10fb9bc97b241c2953e
Adam <arbimer@mac.com>
2022-04-21 17:59:07 -0500

    Fix for task 8

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -1,21 +1,20 @@
 import { Seq } from 'immutable';
 
 function titleCase(str) {
-  return [str.charAt(0).toUpperCase(), str.slice(1)].join('');
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 export default function printBestStudents(object) {
-  const filteredObject = Object.fromEntries(
-    Object.keys(object)
-      .filter((k) => object[k].score >= 70)
-      .map((k) => [
-        k,
-        {
-          ...object[k],
-          firstName: titleCase(object[k].firstName),
-          lastName: titleCase(object[k].lastName),
-        },
-      ])
-  );
+  const filteredArray = Object.keys(object)
+    .filter((k) => object[k].score >= 70)
+    .map((k) => [
+      k,
+      {
+        ...object[k],
+        firstName: titleCase(object[k].firstName),
+        lastName: titleCase(object[k].lastName),
+      },
+    ]);
+  const filteredObject = Object.fromEntries(filteredArray);
   console.log(Seq(filteredObject).toJS());
 }
