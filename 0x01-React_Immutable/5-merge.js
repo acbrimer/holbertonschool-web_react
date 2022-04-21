f48aa7f430c7f6a0a38a3deec0cd7498dc80c31a
Adam <arbimer@mac.com>
2022-04-21 17:38:37 -0500

    Added solution for task 5

---
+++ 0x01-React_Immutable/5-merge.js
@@ -0,0 +1,5 @@
+import { List, Map } from 'immutable';
+
+export const concatElements = (page1, page2) => List([...page1, ...page2]);
+
+export const mergeElements = (page1, page2) => Map({ ...page1, ...page2 });
