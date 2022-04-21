31d6b9a29d00a10ea25d5896fd5fbabada9dd2e9
Adam <arbimer@mac.com>
2022-04-21 16:29:47 -0500

    Added notification actions/action creators for task 5

---
+++ 0x02-react_redux_action_creator_normalizr/task_5/README.md
@@ -0,0 +1,3 @@
+# Task 5
+
+Create types/actions for UI
---
+++ 0x02-react_redux_action_creator_normalizr/task_5/dashboard/src/CourseList/CourseListRow.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { assert } from 'chai';
+import { shallow } from '../../config/setupTests';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+import CourseListRow from './CourseListRow';
+
+describe('<CourseListRow />', () => {
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+  afterEach(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
+  it('Renders thead w/ colspan = 2 if isHeader & no textSecondCell', () => {
+    const row = shallow(
+      <CourseListRow isHeader textFirstCell="First Header" />
+    );
+    assert.equal(row.find('th').length, 1);
+  });
+  it('Renders thead w/ colspan = 2 if isHeader & textSecondCell', () => {
+    const row = shallow(
+      <CourseListRow
+        isHeader={true}
+        textFirstCell="First Header"
+        textSecondCell="Second Header"
+      />
+    );
+    assert.equal(row.find('th').length, 3);
+  });
+  it('Renders tr w/ 2 td elements if not header', () => {
+    const row = shallow(
+      <CourseListRow
+        isHeader={false}
+        textFirstCell="First Cell"
+        textSecondCell="Second Cell"
+      />
+    );
+    assert.equal(row.find('td').length, 2);
+  });
+});
